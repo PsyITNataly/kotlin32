ae5561e0eb763e7e9ab732551c3e77b737b1605f
Natasha O <photographie@yandex.ru>
2023-03-19 01:17:29 +0300

    Second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,42 @@
 fun main() {
 
-    val result = (commission("Visa", 1000_000, 0));
+    val result = (commission("Maestro"));
     println(result);
 
 }
-fun commission(typeCart: String, transfer: Int, previos: Int): Int {
+
+fun commission(typeCart: String): Int {
     return when {
-        typeCart == "MasterCart" -> MasterMaestro(1000, 0)
-        typeCart == "Maestro" -> MasterMaestro(1000,0)
-        typeCart == "Visa" -> VisaMir(1000)
-        typeCart == "Мир" -> VisaMir(1000)
+        typeCart == "MasterCart" || typeCart == "Maestro" -> MasterMaestro(80_000, 50_000)
+        typeCart == "Visa" || typeCart == "Мир" -> VisaMir(2_000, 0)
+        typeCart == "VKPay" -> VKPay(36_000, 5_000)
         else -> 0
     }
 }
+
 fun MasterMaestro(transfer: Int, previos: Int): Int {
     return when {
-        transfer + previos > 75_000 -> transfer * 6 / 1000 + 20
+        transfer + previos > 600_000 -> -200
+        transfer + previos > 150_000 -> -100
+        transfer > 75_000 -> transfer * 6 / 1000 + 20
+
         else -> 0
     }
 }
-fun VisaMir(transfer: Int): Int {
+
+fun VisaMir(transfer: Int, previos: Int): Int {
     return when {
+        transfer + previos > 600_000 -> -200
+        transfer + previos > 150_000 -> -100
         transfer * 75 / 10000 > 35 -> transfer * 75 / 10000
         else -> 35
+    }
+}
+
+fun VKPay(transfer: Int, previos: Int): Int {
+    return when {
+        transfer + previos in 15001..39999 -> -100
+        transfer + previos > 40_000 -> -200
+        else -> 0
     }
 }
